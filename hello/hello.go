be176ed70df4dde3f3bb149bc0b1eff24e744414
TungLuu <tunglt185@gmail.com>
2020-04-10 14:22:40 +0700

    add hello folder again

---
+++ hello/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/kid-coder/stringutil"
+)
+
+func main() {
+	fmt.Println("Hello")
+	fmt.Println("Hello, world.")
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+}
